bc2492b3c8255b5c65e2b4f32d542d9e90e71f12
Cyanic76 <47304192+Cyanic76@users.noreply.github.com>
2021-06-13 15:02:53 +0000

    added replit file

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login(process.env.TOKEN);
+client.login(process.env['TOKEN']);
 
 client.on("ready", () => {
   console.log(`@someone is ready!`);
@@ -11,6 +11,7 @@ client.on("message", message => {
   if(message.mentions.users.first().id === client.user.id || message.mentions.users.first().id === client.id || message.content.includes("@someone")){
     let members = [];
     message.guild.members.cache.map(member => {
+      if(member.user.bot) return;
       members.push(member.user.id);
     });
     let guildMember = members[Math.floor(Math.random()*members.length)]
