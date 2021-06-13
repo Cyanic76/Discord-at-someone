19085922d0d524a678a561be918627aa4c21585b
Cyanic76 <cyanic076@gmail.com>
2021-06-13 16:53:45 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+  console.log(`@someone is ready!`);
+})
+
+client.on("message", message => {
+  if(message.mentions.users.first().id === client.user.id || message.mentions.users.first().id === client.id || message.content.includes("@someone")){
+    let members = [];
+    message.guild.members.cache.map(member => {
+      members.push(member.user.id);
+    });
+    let guildMember = members[Math.floor(Math.random()*members.length)]
+    message.channel.send(`<@${guildMember}>`)
+    return;
+  }
+});
