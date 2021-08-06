3dc377935bdb93dd06bed38a35e899e4e76556cd
Cyanic76 <47304192+Cyanic76@users.noreply.github.com>
2021-08-06 09:53:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,11 @@ client.on("message", message => {
   if(message.author.bot) return;
   const p = message.mentions.users.first();
   if(p && p.id === client.user.id || p && p.id === client.id){
+    var args = message.content.split(" ").slice(1)
+    if(args[0] === 'help'){
+      message.channel.send("Hello there, I'm the @someone bot.\nWhen you ping me, I randomly choose a member from your server and ping them. It can even be a bot.\nTo invite me and learn more about me, follow this link:\nhttps://github.com/Cyanic76/Discord-at-someone#README")
+      return;
+    }
     let members = [];
     message.guild.members.cache.map(member => {
       members.push(member.user.id);
